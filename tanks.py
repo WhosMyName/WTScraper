885b5aadc18cb075ebf46ca26118339529f73cc5
Who is my Name! <chikangwu95@gmail.com>
2022-07-06 18:56:23 +0200

    added VehicleClasses, set defaults, added/sorted modifications

--- tanks.py
+++ tanks.py
@@ -1,41 +1,53 @@
 """ Class description of a ground vehicle from WarThunder
 """
 
+from enum import Enum
 from armament import Armament
 from typing import Dict, Tuple, List
+
+class VehicleClass(Enum):
+    DEFAULT = 1
+    LIGHT = 2
+    MEDIUM = 3
+    HEAVY = 4
+    SPG = 5
+    SPAA = 6
 
 class Tank():
     """Class that represents a WarThunder Ground Vehicle
     """
-    name: str
-    armaments: List[Armament]
-    armour_hull: Tuple[int, int, int]
-    armour_turret: Tuple[int, int, int]
-    crew: int
-    visibility: int
-    #stabilizer: int # 0 planes 1 plane  2 plane
-    weight: float
+    name: str = "Generic Ground Vehicle"
+    vehicle_class: VehicleClass = VehicleClass.DEFAULT
+    armaments: List[Armament] = []
+    armour: Tuple[int, int, int] = (-1, -1, -1)
+    armour_hull: Tuple[int, int, int] = (-1, -1, -1)
+    armour_turret: Tuple[int, int, int] = (-1, -1, -1)
+    crew: int = -1
+    visibility: int = -1
+    weight: float = -1.0
     engine_power: Dict = {"RB": 0, "AB": 0}
     max_speed_forward: Dict = {"RB": 0, "AB": 0, "SB": 0}
     max_speed_reverse: Dict = {"RB": 0, "AB": 0, "SB": 0}
     gears: Dict = {"Forward": 0, "Back": 0}
     power_to_weight: Dict = {"RB": 0, "AB": 0}
-    rank: int
-    battle_rating: float
-    is_amphibious:bool
+    rank: int = -1
+    battle_rating: Dict = {"RB": 0, "AB": 0, "SB": 0}
+    is_amphibious:bool = False
     # Mods
     # Utility
-    smokes: int
-    ess: bool
-    artillery: bool
-    dozer_blade: bool
+    smokes: bool = False
+    ess: bool = False
+    artillery: bool = False
+    dozer_blade: bool = False
+    scouting: bool = False
     # Visuals
-    night_vision: bool
-    thermal_vision: bool
+    night_vision: bool = False
+    thermal_vision: bool = False
     # Rangefinding
-    rangefinder: bool
-    laser_rangefinder: bool
-    laser_warning_rangefinder: bool
+    rangefinder: bool = False
+    laser_rangefinder: bool = False
+    laser_warning_rangefinder: bool = False
+
 
     def __init__(self, name: str):
         """
@@ -45,21 +57,6 @@ class Tank():
             Ground Vehicle Name
         """
         self.name = name
-        self.armaments: List[Armament] = []
-        self.armour_hull: Tuple[int, int, int] = ()
-        self.armour_turret: Tuple[int, int, int] = ()
-        self.crew: int = -1
-        self.visibility: int = -1
-        self.smokes: int = -1
-        self.ess: bool = False
-        self.stabilizer: int = -1 # 0 planes 1 plane  2 plane
-        self.weight: float = -100.0
-        self.engine_power: Dict = {"RB": 0, "AB": 0}
-        self.max_speed_forward: Dict = {"RB": 0, "AB": 0, "SB": 0}
-        self.max_speed_reverse: Dict = {"RB": 0, "AB": 0, "SB": 0}
-        self.gears: Dict = {"Forward": 0, "Back": 0}
-        self.power_to_weight: Dict = {"RB": 0, "AB": 0}
-        self.night_vision: bool = False
-        self.thermal_vision: bool = False
 
-
+    def __str__(self) -> str:
+        return f"{self.name} with Armaments [{' || '.join(armament.__str__() for armament in self.armaments)}]"
