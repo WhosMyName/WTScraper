150381f809930819a4206ae93ed00c02cad9825c
Who is my Name! <chikangwu95@gmail.com>
2022-07-15 19:46:09 +0200

    added parsing for BR/Ran/Prem/Squad, fixed atgm, more features

--- wt_wiki_ground_parser.py
+++ wt_wiki_ground_parser.py
@@ -4,7 +4,7 @@
 from bs4 import BeautifulSoup, SoupStrainer, Tag
 from ammunition import Ammunition
 from armament import Armament, Stabilizer
-from tanks import Tank
+from tanks import Tank, VehicleClass
 from typing import List
 
 
@@ -25,23 +25,66 @@ def parse_ground_vehicle(response_content: str) -> Tank:
 
     # TODO:
     # Horizontal Guidance
-    # Rank
-    # BR
     # Radar
     # Scout (confused shrug or done)
-    # ERA
     # Variable Suspension ? (hydropneumatic suspension)
     # APS ?
-    # Reverse Gearbox
+    # Composite armour
 
     soup = BeautifulSoup(response_content, "html.parser", parse_only=SoupStrainer(class_="mw-parser-output"))
     specs = list(soup.find_all(class_="specs_info"))
     unparsed_armaments: List[Armament] = []
 
+    # General Tank Parsing (Name, VehicleClass, Premium, Squadron)
+    parsed_tank = Tank(name=soup.find(class_="general_info_name").text)
+    parsed_tank.is_squadron = True if soup.find(class_="squadron") else False
+    parsed_tank.is_premium = True if soup.find(class_="premium") else False
+    if parsed_tank.is_squadron:
+        soup.find(class_="squadron").extract()
+    if parsed_tank.is_premium:
+        soup.find(class_="premium").extract()
+    vehicle_class = soup.find(class_="general_info_class").find("a").text.strip() # checking if the vehicle is of a certiain class and/or premium/squadron
+    if vehicle_class == "Light tank":
+        parsed_tank.vehicle_class = VehicleClass.LIGHT
+    elif vehicle_class == "Medium tank":
+        parsed_tank.vehicle_class = VehicleClass.MEDIUM
+    elif vehicle_class == "Heavy tank":
+        parsed_tank.vehicle_class = VehicleClass.HEAVY
+    elif vehicle_class == "SPAA":
+        parsed_tank.vehicle_class = VehicleClass.SPAA
+    elif vehicle_class == "Tank destroyer":
+        parsed_tank.vehicle_class = VehicleClass.SPG
+    else:
+        print(f"{parsed_tank.name}: {vehicle_class}")
+
+    # Battle Rating, Rank, Nation
+    parsed_tank.nation = soup.find(class_="general_info_nation").find_all("a")[-1].text.strip()
+    text_rank = soup.find(class_="general_info_rank").a.text.strip().split(" ")[0] # "V Rank"
+    if text_rank == "I": # maybe I won't need to touch this for the next 5-10 years
+        parsed_tank.rank = 1
+    elif text_rank == "II":
+        parsed_tank.rank = 2
+    elif text_rank == "III":
+        parsed_tank.rank = 3
+    elif text_rank == "IV":
+        parsed_tank.rank = 4
+    elif text_rank == "V":
+        parsed_tank.rank = 5
+    elif text_rank == "VI":
+        parsed_tank.rank = 6
+    elif text_rank == "VII":
+        parsed_tank.rank = 7
+    elif text_rank == "VIII":
+        parsed_tank.rank = 8
+    elif text_rank == "IX":
+        parsed_tank.rank = 9
+    elif text_rank == "X":
+        parsed_tank.rank = 10
+    battle_rating  = [float(br.text.strip()) for br in soup.find(class_="general_info_br").find_all("td")[3:]]# grab all table entries, use only the last 3 as they contain the BR 
+    parsed_tank.battle_rating = {"AB": battle_rating[0], "RB": battle_rating[1], "SB": battle_rating[2]}
+
     # Armament Parsing
     unparsed_armaments = [spec for spec in specs if len(spec["class"]) == 2] # specs_info weapons
-    specs = [spec for spec in specs if len(spec["class"]) == 1 ] # specs_info
-    parsed_tank = Tank(name=soup.find(class_="general_info_name").text)
     for armament in unparsed_armaments:
         parsed_tank.armaments.append(parse_ground_armaments(armament))
   
@@ -52,7 +95,7 @@ def parse_ground_vehicle(response_content: str) -> Tank:
         if len(tables[iterator]["class"]) == 2:
             if tables[iterator - 1].find("tr").find("th").find("a"):
                 armament_name = tables[iterator - 1].find("tr").find("th").find("a").text.strip()
-            print(bytes(armament_name, encoding="utf-8"))
+            #print(bytes(armament_name, encoding="utf-8"))
             if tables[iterator].find("tr").find("th").text.strip() == "Penetration statistics":
                 ammunitions = parse_ground_ammunitions_pen(ammo_pen_specs=tables[iterator])
             elif tables[iterator].find("tr").find("th").text.strip() == "Shell details" and len(ammunitions):
@@ -60,16 +103,15 @@ def parse_ground_vehicle(response_content: str) -> Tank:
             else:
                 pass
             for armament in parsed_tank.armaments:
-                print(f"Armament:{bytes(armament.name, encoding='utf-8')}:{bytes(armament_name, encoding='utf-8')}")
+                #print(f"Armament:{bytes(armament.name, encoding='utf-8')}:{bytes(armament_name, encoding='utf-8')}")
                 if armament_name in armament.name:
                     armament.ammo_types = ammunitions
 
-    for armament in parsed_tank.armaments:
-        print(armament.__dict__)
-
     # Vehicle Spec Parsing
+    specs = [spec for spec in specs if len(spec["class"]) == 1 ] # specs_info
     for spec in specs:
-        print(f"{spec}\n\n")
+        #print(f"{spec}\n\n")
+        pass
         # parse Armour, Hull, turret, crew, visibility
         # parse speed, gears, weight, engine power, power/weight
         # parse repair cost, crew training, battle reward, 
@@ -79,9 +121,15 @@ def parse_ground_vehicle(response_content: str) -> Tank:
     for feature in soup.find_all(class_="feature_name"):
         if feature.text.strip() == "Amphibious":
             parsed_tank.is_amphibious = True
+        elif feature.text.strip() == "ERA":
+            parsed_tank.era = True
+        elif feature.text.strip() == "Reverse gearbox":
+            parsed_tank.reverse_gearbox = True
+        elif feature.text.strip() == "Controlled suspension":
+            parsed_tank.controlled_suspension = True
         elif "stabilizer" in feature.text.strip() or feature.text.strip() == "Autoloader" : # keep this here to catch all the funky stuff
             pass
-        elif feature.text.strip() in ["Smoke grenades", "ESS", "Laser rangefinder", "Night vision device"]: # we got those parsed already, so no need for warnings
+        elif feature.text.strip() in ["Smoke grenades", "ESS", "Laser rangefinder", "Night vision device", "Rangefinder", "Self-entrenching equipment"]: # we got those parsed already, so no need for warnings
             pass
         else:
             print(f"Feature WARN: {feature.text.strip()}") # add logging here
@@ -131,18 +179,24 @@ def parse_ground_ammunitions_stats(ammo_specs: Tag, ammunitions: List[Ammunition
     round_specs = ammo_specs.find_all("tr")[3:]
     for round in round_specs:
         for ammo in ammunitions:
+            #check for ATGMs with extra range -> all other values shift by 1
+            # range position: [3]
             round_data = round.find_all("td")
             if round_data[0].text.strip()!= ammo.name:
                 continue
-            ammo.velocity = int(round_data[2].text.replace(",", ""))
-            ammo.projectile_mass = float(round_data[3].text.replace(",", ""))
-            ammo.ricochet = dict(zip(["0%", "50%","100%"], [int(angle.text.replace("°", "")) for angle in round_data[7:]]))
-            if not round_data[4].text.strip() == "N/A":
-                ammo.fuse_delay = float(round_data[4].text)
-            if not round_data[5].text.strip() == "N/A":
-                ammo.fuse_sensetivity = float(round_data[5].text)
-            if not round_data[6].text.strip() == "N/A":
-                ammo.explosive_mass = float(round_data[6].text.replace(",", ""))
+            atgm_offset = 0
+            if "ATGM" in round_data[1].text.strip(): # check if the round is a ATGM missile and apply offset if needed
+                atgm_offset = 1
+                ammo.range = int(round_data[2].text.strip().replace(",", ""))
+            ammo.velocity = int(round_data[2+atgm_offset].text.replace(",", ""))
+            ammo.projectile_mass = float(round_data[3+atgm_offset].text.replace(",", ""))
+            ammo.ricochet = dict(zip(["0%", "50%","100%"], [int(angle.text.replace("°", "")) for angle in round_data[7+atgm_offset:]]))
+            if not round_data[4+atgm_offset].text.strip() == "N/A":
+                ammo.fuse_delay = float(round_data[4+atgm_offset].text)
+            if not round_data[5+atgm_offset].text.strip() == "N/A":
+                ammo.fuse_sensetivity = float(round_data[5+atgm_offset].text)
+            if not round_data[6+atgm_offset].text.strip() == "N/A":
+                ammo.explosive_mass = int(round_data[6+atgm_offset].text.replace(",", ""))
 
 
 
@@ -164,7 +218,7 @@ def parse_ground_ammunitions_pen(ammo_pen_specs: Tag) -> List[Ammunition]:
     for ammo in ammunitions:
         round_name = ammo.find_all("td")[0].text.strip()
         round_type = ammo.find_all("td")[1].text.strip()
-        round_pen_at_distance = dict(zip(["10", "100", "500", "1000", "1500", "2000"], [int(x.text) for x in ammo.find_all("td")[2:]]))
+        round_pen_at_distance = dict(zip(["10", "100", "500", "1000", "1500", "2000"], [int(x.text.strip().replace(",", "")) for x in ammo.find_all("td")[2:]]))
         parsed_ammo.append(Ammunition(name=round_name, ammo_type=round_type, pen_at_distance=round_pen_at_distance))
     return parsed_ammo
 
@@ -203,26 +257,20 @@ def parse_ground_armaments(armament_specs: Tag) -> Armament:
     armament = Armament(name=armament_name)
     for stat in armament_specs.find_all(class_="specs_char_block"):
         if stat.find(class_="name").text.strip() == "Belt capacity":
-            print("BCap")
             armament.belt_capacity = int(stat.find(class_="value").text.split(" rounds")[0].replace(" ", ""))
         elif stat.find(class_="name").text.strip() == "Ammunition":
-            print("Cap")
             armament.capacity = int(stat.find(class_="value").text.split(" rounds")[0].replace(" ", ""))
         elif stat.find(class_="name").text.strip() == "Fire rate":
-            print("Rate")
             armament.fire_rate = int(stat.find(class_="value").text.split(" shots")[0].replace(" ", ""))
         elif stat.find(class_="name").text.strip() == "First-order":
-            print("Stowage")
             armament.first_stowage = int(stat.find(class_="value").text.split(" rounds")[0].replace(" ", ""))
         elif stat.find(class_="name").text.strip() == "Vertical guidance":
-            print("guidance")
             guidances = [int(x) for x in stat.find(class_="value").text.replace("°", "").split(" / ")]
             if guidances[0] > guidances[1]:
                 armament.vertical_guidance = {"positive": guidances[0], "negative": guidances[1]}
             else:
                 armament.vertical_guidance = {"positive": guidances[1], "negative": guidances[0]}
         elif stat.find(class_="name").text.strip() == "Reload":
-            print("reloads")
             try:
                 if stat.find(class_="specs_char_line indent"): # i'd guess this case is used if there's no autoloader
                     reloads = [float(x) for x in stat.find(class_="specs_char_line indent").find(class_="value").text.rstrip(" s").split(" → ")]
@@ -251,10 +299,8 @@ def parse_ground_armaments(armament_specs: Tag) -> Armament:
         elif feature.text.strip() == "Shoulder stabilizer":
             armament.stabilizer = Stabilizer.SHOULDER
         else:
-            print(f"Feature WARN: {feature.text.strip()}") # add logging here
+            print(f"Armament Feature WARN: {feature.text.strip()}") # add logging here
 
-
-    print(armament.__dict__)
     return armament
 
 
@@ -264,21 +310,23 @@ def __main__():
     """
     content: str
     test_vehicles = [
-#        "M24_(Italy)", # Vertical Stabilizer, Name Parsing Italy
-#        "AML-90_(Israel)", # Name parsing Israeal
-#        "Magach_3_(USA)", # Name Parsing USA
-#        "Maus", # Multi Cannon
-#        "Pz.Kpfw._Churchill_(Germany)", # "Shoulder Stabilizer", German Name Parsing
-#        "Type_62_(USSR)", # Name Parsing USSR
-#        "Sho't_Kal_Dalet_(Great_Britain)", # Name Parsing GB
-#        "M47_(Japan)", # Name Parsing JP, Rangefinder
-#        "PT-76_(China)", # Name Parsing Taiwan
-#        "ItO_90M_(France)", # Name Parsing France
-#        "Bkan_1C", # Reverse Gearbox
-#        "AMX-10RC", # Suspension
-        "Object_685", # Amphibious, Autoloader
-        "T-72AV_(TURMS-T)", # ERA, ESS, Dozer Blades
-        "Centauro_I_105", # LWS, Thermals
+        # "M24_(Italy)", # Vertical Stabilizer, Name Parsing Italy
+        # "AML-90_(Israel)", # Name parsing Israeal
+        # "Magach_3_(USA)", # Name Parsing USA
+        # "Maus", # Multi Cannon
+        # "Pz.Kpfw._Churchill_(Germany)", # "Shoulder Stabilizer", German Name Parsing
+        # "Type_62_(USSR)", # Name Parsing USSR
+        # "Sho't_Kal_Dalet_(Great_Britain)", # Name Parsing GB
+        # "M47_(Japan)", # Name Parsing JP, Rangefinder
+        # "PT-76_(China)", # Name Parsing Taiwan
+        # "ItO_90M_(France)", # Name Parsing France
+        # "Bkan_1C", # Reverse Gearbox
+        # "AMX-10RC", # Suspension
+        # "Object_685", # Amphibious, Autoloader
+        # "T-72AV_(TURMS-T)", # ERA, ESS, Dozer Blades
+        # "Centauro_I_105", # LWS, Thermals
+        # "ADATS_(M113)", # ATGM
+        "BMP-2M", # Squadron
         "ZSU-23-4", # Radar in Wiki
         "SIDAM_25", # Optotronics
         "VEAK_40", # Radar not in Wiki
