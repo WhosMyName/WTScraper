a16f6d71a61e8bfe02ed38f697228768806ab1a1
Who is my Name! <chikangwu95@gmail.com>
2022-08-26 13:05:10 +0200

    halted usage of abbreviations in dicts and added remaining class members

--- tanks.py
+++ tanks.py
@@ -31,22 +31,35 @@ class Tank():
         self.is_premium: bool = False
         self.is_squadron: bool = False
         self.rank: int = -1
-        self.battle_rating: Dict = {"AB": 0, "RB": 0, "SB": 0}
+        self.battle_rating: Dict[str, float] = {"Arcade": 0.0, "Realistic": 0.0, "Simulator": 0.0}
         self.armaments: List[Armament] = []
+        self.cost: int = -1
+        self.research: int = -1
+        self.cost_usd: int = -1
+        self.cost_ge: int = -1
+        self.repair_cost_stock: Dict[str, int] = {"Arcade": -1, "Realistic": -1, "Simulator": -1}
+        self.repair_cost_upgraded: Dict[str, int] = {"Arcade": -1, "Realistic": -1, "Simulator": -1}
+        self.total_cost_modifications_sl: int = -1
+        self.total_cost_modifications_rp: int = -1
+        self.crew_training: int = -1
+        self.talisman_cost: int = -1
+        self.rewards_sl: Dict[str, int] = {"Arcade": -1, "Realistic": -1, "Simulator": -1}
+        self.rewards_rp: Dict[str, int] = {"Arcade": -1, "Realistic": -1, "Simulator": -1}
         # Armour
-        self.armour: Tuple[int, int, int] = (-1, -1, -1)
-        self.armour_hull: Tuple[int, int, int] = (-1, -1, -1)
-        self.armour_turret: Tuple[int, int, int] = (-1, -1, -1)
+        self.armour_hull: Dict[str, int] = {"front": -1, "side": -1, "back": -1}
+        self.armour_turret: Dict[str, int] = {"front": -1, "side": -1, "back": -1}
         # Stuff
         self.crew: int = -1
         self.visibility: int = -1
         self.weight: float = -1.0
         # Movement and Engine
-        self.engine_power: Dict = {"RB": 0, "AB": 0}
-        self.max_speed_forward: Dict = {"RB": 0, "AB": 0, "SB": 0}
-        self.max_speed_reverse: Dict = {"RB": 0, "AB": 0, "SB": 0}
-        self.gears: Dict = {"Forward": 0, "Back": 0}
-        self.power_to_weight: Dict = {"RB": 0, "AB": 0}
+        self.gears: Dict[str, int] = {"Forward": 0, "Back": 0}
+        self.max_speed_forward: Dict[str, int] = {"Realistic": 0, "Arcade": 0}
+        self.max_speed_reverse: Dict[str, int] = {"Realistic": 0, "Arcade": 0}
+        self.power_to_weight_stock: Dict[str, float] = {"Realistic": 0, "Arcade": 0}
+        self.power_to_weight_upgraded: Dict[str, float] = {"Realistic": 0, "Arcade": 0}
+        self.engine_power_stock: Dict[str, int] = {"Realistic": 0, "Arcade": 0}
+        self.engine_power_upgraded: Dict[str, int] = {"Realistic": 0, "Arcade": 0}
         # Features
         self.era: bool = False
         self.is_amphibious:bool = False
